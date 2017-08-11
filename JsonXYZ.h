4cf02ffe65e1b926b5ac4106fcc151e716a9daf6
carvajavo <javiercarva@gmail.com>
2017-08-11 15:55:24 -0600

    Update de Biblioteca para IDE Arduino

---
+++ JsonXYZ.h
@@ -0,0 +1,26 @@
+#ifndef JSON_XYZ
+#define JSON_XYZ
+
+#include <Arduino.h>
+
+void jsonInit(String *json);
+
+void jsonClose(String *json);
+
+void jsonClear(String *json);
+
+void addToJson(String *json, String id, String value);
+
+void addToJsonAux(String *json, String id, String value);
+
+void addToJson(String *json, String id, int value);
+
+void addToJson(String *json, String id, float value);
+
+String getJsonStr(String input, String attribute);
+
+int getJsonInt(String input, String attribute);
+
+float getJsonFloat(String input, String attribute);
+
+#endif
